bb71e06fb02b1de16eb880e0103e0b5a9800dc5a
sean-huberman <huberman.sean@gmail.com>
2022-12-15 16:44:42 -0500

    Added Location Simulator routing and geofence activity.

--- Sample_Java_Project/app/src/main/java/com/mapsted/sample/SampleMainLaunchActivity.java
+++ Sample_Java_Project/app/src/main/java/com/mapsted/sample/SampleMainLaunchActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mapsted.sample.activities.LocationSimulatorRoutingAndGeofenceActivity;
 import com.mapsted.sample.activities.SampleActivityWithFragment;
 import com.mapsted.sample.activities.SampleMapActivity;
 import com.mapsted.sample.activities.SampleMapUiWithAButtonActivity;
@@ -19,11 +20,11 @@ public class SampleMainLaunchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_main_launch);
         SampleMainLaunchActivity context = this;
+
         findViewById(R.id.btn_launch_map_with_tools).setOnClickListener(v -> {
             Intent intent = new Intent(context, SampleMapWithUiToolsActivity.class);
             context.startActivity(intent);
         });
-
 
         findViewById(R.id.btn_launch_map_with_button).setOnClickListener(v -> {
             Intent intent = new Intent(context, SampleMapUiWithAButtonActivity.class);
@@ -42,6 +43,11 @@ public class SampleMainLaunchActivity extends AppCompatActivity {
 
         findViewById(R.id.btn_launch_map).setOnClickListener(v -> {
             Intent intent = new Intent(context, SampleMapActivity.class);
+            context.startActivity(intent);
+        });
+
+        findViewById(R.id.btn_launch_loc_simulator).setOnClickListener(v -> {
+            Intent intent = new Intent(context, LocationSimulatorRoutingAndGeofenceActivity.class);
             context.startActivity(intent);
         });
     }
